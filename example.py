b5e9f3fee3524c0394da0417eadeba06fc7c5067
Habib2014 <ahcastalia@gmail.com>
2020-04-07 16:12:12 +0100

    modif  example.py

--- example.py
+++ example.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 # Number of independent experiments in each trial
 n = 50
 # Probability of success for each experiment
-p = 0.5
+p = 0.1
 fig, ax = plt.subplots(1, 1)
 x = range(0,50)
 ax.plot(x, binom.pmf(x, n, p), 'ro', label='actual binomial distribution')
